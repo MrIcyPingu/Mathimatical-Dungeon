8f0a993da817b8240112c92258ac14ad1b43778a
MrIcyPingu <160473397+MrIcyPingu@users.noreply.github.com>
2024-02-27 18:21:49 +0000

    Update Battle_encounter.py
    made into callable class for main

--- Battle_encounter.py
+++ Battle_encounter.py
@@ -3,22 +3,6 @@ import tkinter.font as tkFont
 import tkinter.messagebox
 import random
 
-#card_focus()
-#this function is used to enlarge a card to make it stand out in the hand
-#@param - button - tk.button - the card to be focused on
-def card_focus(button: tk.Button):
-        button.tkraise()
-        button.place(y=330,width=162,height=172)
-
-#card_unfocus()
-#this function is used to return a card to its orignal size in the correct postion on the deck
-#@param - button - tk.button - the card to be unfocused
-def card_unfocus(button: tk.Button):
-        card_num=int(str(button.winfo_name).split(".")[-1][-3]) #used to determine which layer the card should go on 
-        if card_counter > 0 and card_num < card_counter - 1:
-             card_below = cards[card_num + 1]
-             button.lower(card_below)
-        button.place(y=430,width=81,height=86)
 
 #Entity
 #A class for the creation of entities that will be used the battle encounter
@@ -32,9 +16,31 @@ class Entity:
      energy_limit = 4
      energy = 4
 
-#Entity
+#Battle_Encounter
 #A class for the battle encounter window
-class Battle_Encounter:
+class Battle_Encounter():
+    player = Entity() #creates the player and enemy
+    enemy = Entity()
+    card_counter = 0
+    cards = []
+
+    #card_focus()
+    #this function is used to enlarge a card to make it stand out in the hand
+    #@param - button - tk.button - the card to be focused on
+    def card_focus(self, button: tk.Button):
+            button.tkraise()
+            button.place(y=330,width=162,height=172)
+
+    #card_unfocus()
+    #this function is used to return a card to its orignal size in the correct postion on the deck
+    #@param - button - tk.button - the card to be unfocused
+    def card_unfocus(self, button: tk.Button):
+            card_num=int(str(button.winfo_name).split(".")[-1][-3]) #used to determine which layer the card should go on 
+            if self.card_counter > 0 and card_num < self.card_counter - 1:
+                card_below = self.cards[card_num + 1]
+                button.lower(card_below)
+            button.place(y=430,width=81,height=86)
+
     def __init__(self, root):
         #setting title
         root.title("Battle")
@@ -55,66 +61,76 @@ class Battle_Encounter:
         enemy_frame = tk.Frame(root, width=128,height=184, borderwidth=2, relief="groove")
         enemy_frame.place(x=420,y=140)
 
+        self.Health_lb=tk.Label(root, font=tkFont.Font(family='Times',size=10),fg="#333333", justify="left", text= "Health: " + str(self.player.health) + " defence: " + str(self.player.defence))
+        self.Health_lb.place(x=10,y=20,width=170,height=25) #creates the player health label
+
+        self.Health_Enemy_lb=tk.Label(root, font=tkFont.Font(family='Times',size=10),fg="#333333", justify="left", text= "Health: " + str(self.enemy.health) + " defence: " + str(self.enemy.defence))
+        self.Health_Enemy_lb.place(x=380,y=160,width=170,height=25) #creates the eneny health label
+
+        self.Energy_lb=tk.Label(root, font=tkFont.Font(family='Times',size=10),fg="#333333", justify="right", text="Energy: " + str(self.player.energy))
+        self.Energy_lb.place(x=500,y=20,width=70,height=25) #creates the player energy label
+
         #Give the first hand of cards
-        while card_counter <= 5:
-            self.New_card()
+        while self.card_counter <= 5:
+            self.New_card(root)
 
         #End turn button
-        End_turn_btn = tk.Button(root, bg="#f0f0f0", font=tkFont.Font(family='Times',size=10), fg="#000000", justify="center", text="End turn", command= lambda: self.End_turn_btn())
+        End_turn_btn = tk.Button(root, bg="#f0f0f0", font=tkFont.Font(family='Times',size=10), fg="#000000", justify="center", text="End turn", command= lambda: self.End_turn_btn(root))
         End_turn_btn.place(x=250,y=20,width=70,height=25)
 
         #Restarts the health of entities
-        enemy.health = 10
-        player.health = 10
+        self.enemy.health = 10
+        self.player.health = 10
         
     #End_turn_btn()
     #Ends the turn of the user and performs the enemy's turn
-    def End_turn_btn(self):
-         enemy.defence = 0
-         while enemy.energy >= 1:
+    def End_turn_btn(self, window:tk.Tk):
+         self.enemy.defence = 0
+         while self.enemy.energy >= 1:
               match random.randint(0, 3): #randomly gets the enemy to attack or defend until their energy runs out
                  case 1:
-                    self.attack(enemy, player)
+                    self.attack(self.enemy, self.player, window=window)
                  case 2:
-                    self.defend(enemy)
+                    self.defend(self.enemy)
                  case _:
-                    self.attack(enemy, player)
-         enemy.energy = 4
-         player.energy = 4 #resets both of entities energy
-         player.defence = 0
-         global cards
-         for card in cards:
+                    self.attack(self.enemy, self.player, window=window)
+         self.enemy.energy = 4
+         self.player.energy = 4 #resets both of entities energy
+         self.Energy_lb["text"] = "Energy: " + str(self.player.energy)
+         self.player.defence = 0
+         for card in self.cards:
              card.destroy() #give the user a new hand of cards
-         global card_counter 
-         card_counter = 0
-         while card_counter <= 5:
-            self.New_card()
+         self.card_counter = 0
+         while self.card_counter <= 5:
+            self.New_card(window)
 
     #attack()
     #Used to attack an entity
     #@param - attacker - Entity - the attacking entity
     #@param - defender - Entity - the defending entity
     #@param - card - tk.Button - the card being used
-    def attack(self, attacker: Entity, defender: Entity, card: tk.Button=None):
+    def attack(self, attacker: Entity, defender: Entity, card: tk.Button=None, window:tk.Tk=None):
          if attacker.energy > 0:
             if defender.defence > 0:
                 defender.defence -= 1 # if the defender has a defence value
             else:
                defender.health -= 1 #if the defender has no defence value
             attacker.energy -= 1 #takes 1 away from the attacker energy
-            if defender.health == 0 and defender == enemy:
+            if defender.health == 0 and defender == self.enemy:
                 tkinter.messagebox.showinfo("You win",  "the enemy's health has reached 0. You win") #if the enemies health reach 0
-                root.destroy()
+                window.destroy()
+                return
             elif defender.health == 0:
                 tkinter.messagebox.showinfo("You lose", "your health has reached 0. GAME OVER")#if the users health reach 0
-                root.destroy()
+                window.destroy()
+                return
             if card != None: #sets the labels to the new values
-                Health_Enemy_lb["text"] = "Health: " + str(defender.health) + " defence: " + str(defender.defence)
-                Energy_lb["text"] = "Energy: " + str(attacker.energy)
+                self.Health_Enemy_lb["text"] = "Health: " + str(defender.health) + " defence: " + str(defender.defence)
+                self.Energy_lb["text"] = "Energy: " + str(attacker.energy)
                 card["state"] = "disabled"
             else:
                 try:
-                    Health_lb["text"] = "Health: " + str(defender.health) + " defence: " + str(defender.defence)
+                    self.Health_lb["text"] = "Health: " + str(defender.health) + " defence: " + str(defender.defence)
                 finally:
                     return
                 
@@ -127,12 +143,12 @@ class Battle_Encounter:
             defender.defence += 1 # adds 1 to the defenders defence
             defender.energy -= 1
             if card != None: #sets the labels to the new values
-                Health_lb["text"] = "Health: " + str(defender.health) + " defence: " + str(defender.defence)
-                Energy_lb["text"] = "Energy: " + str(defender.energy)
+                self.Health_lb["text"] = "Health: " + str(defender.health) + " defence: " + str(defender.defence)
+                self.Energy_lb["text"] = "Energy: " + str(defender.energy)
                 card["state"] = "disabled"
             else:
                 try:
-                    Health_Enemy_lb["text"] = "Health: " + str(defender.health) + " defence: " + str(defender.defence)
+                    self.Health_Enemy_lb["text"] = "Health: " + str(defender.health) + " defence: " + str(defender.defence)
                 finally:
                     return
 
@@ -144,51 +160,33 @@ class Battle_Encounter:
          if defender.energy > 0:
             defender.health += 1 #adds 1 to the defenders health
             defender.energy -= 1
-            Health_lb["text"] = "Health: " + str(defender.health) + "defence: " + str(defender.defence) #sets the labels to the new values
-            Energy_lb["text"] = "Energy: " + str(defender.energy)
+            self.Health_lb["text"] = "Health: " + str(defender.health) + " defence: " + str(defender.defence) #sets the labels to the new values
+            self.Energy_lb["text"] = "Energy: " + str(defender.energy)
             card["state"] = "disabled"
 
     #New_card()
     #Used to create a random typed card
-    def New_card(self):
-        global card_counter
+    def New_card(self, window: tk.Tk):
         card_type = 0
-        card = tk.Button(root, name="card_" + str(card_counter), bg="#f0f0f0", font=tkFont.Font(family='Times',size=10), fg="#000000", justify="center")
+        card = tk.Button(window, name="card_" + str(self.card_counter), bg="#f0f0f0", font=tkFont.Font(family='Times',size=10), fg="#000000", justify="center")
         match random.randint(0, 3): #determines the type of card
                  case 0:
                     card["text"] = "Attack"
-                    card["command"] = lambda: self.attack(player, enemy, card)
+                    card["command"] = lambda: self.attack(self.player, self.enemy, card, window)
                  case 1:
                     card["text"] = "Defend"
-                    card["command"] = lambda: self.defend(player, card)
+                    card["command"] = lambda: self.defend(self.player, card)
                  case 2:
                     card["text"] = "Heal"
-                    card["command"] = lambda: self.heal(player, card)
+                    card["command"] = lambda: self.heal(self.player, card)
                  case _:
                     card["text"] = "Attack"
-                    card["command"] = lambda: self.attack(player, enemy, card)
+                    card["command"] = lambda: self.attack(self.player, self.enemy, card)
 
-        cards.append(card)
-        card.place(x=140 + card_counter*50 ,y=430,width=81,height=86)
-        card.bind("<Enter>", lambda x: card_focus(card)) #attaches the focus and unfocus to the card on hover
-        card.bind("<Leave>", lambda x: card_unfocus(card))
-        card_counter += 1
-            
+        self.cards.append(card)
+        card.place(x=140 + self.card_counter*50 ,y=430,width=81,height=86)
+        card.bind("<Enter>", lambda x: self.card_focus(card)) #attaches the focus and unfocus to the card on hover
+        card.bind("<Leave>", lambda x: self.card_unfocus(card))
+        self.card_counter += 1
 
-if __name__ == "__main__":
-    player = Entity() #creates the player and enemy
-    enemy = Entity()
-    card_counter = 0
-    cards = []
-    root = tk.Tk()
-    app = Battle_Encounter(root)
 
-    Health_lb=tk.Label(root, font=tkFont.Font(family='Times',size=10),fg="#333333", justify="left", text= "Health: " + str(player.health) + " defence: " + str(player.defence))
-    Health_lb.place(x=10,y=20,width=170,height=25) #creates the player health label
-
-    Health_Enemy_lb=tk.Label(root, font=tkFont.Font(family='Times',size=10),fg="#333333", justify="left", text= "Health: " + str(enemy.health) + " defence: " + str(enemy.defence))
-    Health_Enemy_lb.place(x=380,y=160,width=170,height=25) #creates the eneny health label
-
-    Energy_lb=tk.Label(root, font=tkFont.Font(family='Times',size=10),fg="#333333", justify="right", text="Energy: " + str(player.energy))
-    Energy_lb.place(x=500,y=20,width=70,height=25) #creates the player energy label
-    root.mainloop()
